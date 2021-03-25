1824bc89f286953294e98b63856e8620a8b69120
Guilherme T Maeoka <gui.mspace@gmail.com>
2021-03-25 07:38:50 -0300

    Add octave files

---
+++ src/envelope_matrix.m
@@ -0,0 +1,35 @@
+function [diag env env_col env_lin] = envelope_matrix (A)
+    diag = diag(A);
+    env = [];
+    env_lin = [];
+    env_col = [1];
+
+    n = size(A)(2);
+
+    for j = 2:1:n
+        i = 1;
+        while (i < j && A(i, j) == 0)
+            i = i + 1;
+        endwhile
+
+        s = 1 + size(env)(2);
+        env_col = [env_col s];
+
+        if (i == j)
+            continue
+        endif
+
+        next = A(i:(j-1), j);
+        next = transpose(next);
+        env = [env next];
+
+        env_lin = [env_lin i:(j-1)];
+    end
+
+    env = [env 0];
+
+    env_lin = [env_lin 0];
+
+    s = size(env)(2);
+    env_col = [env_col s];
+endfunction
---
+++ test/test.m
@@ -0,0 +1,24 @@
+A = eye(6);
+[diag env env_col env_lin] = envelope_matrix(A)
+
+
+A = [
+    11  12  0   14  0   0;
+    0   22  23  0   0   0;
+    0   0   33  0   0   0;
+    0   0   0   44  0   46;
+    0   0   0   0   55  0;
+    0   0   0   0   0   66
+];
+[diag env env_col env_lin] = envelope_matrix(A)
+
+
+A = [
+    11  12  0   0   0   0;
+    0   22  23  0   0   0;
+    0   0   33  0   0   0;
+    0   0   0   44  0   46;
+    0   0   0   0   55  0;
+    0   0   0   0   0   66
+];
+[diag env env_col env_lin] = envelope_matrix(A)
