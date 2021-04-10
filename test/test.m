07d1b4c7f2c38126e197d0e2fc21a21546f2c066
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-04-09 21:32:53 -0300

    Add test case

--- test/test.m
+++ test/test.m
@@ -29,6 +29,19 @@ A = [
 [diagg, env, env_col, env_lin] = lin_envelope(A')
 
 
+
+A = [
+    11  12  0   0   0   0;
+    0   22  23  0   0   0;
+    0   0   33  34  0   0;
+    0   0   0   44  45  0;
+    0   0   0   0   55  56;
+    0   0   0   0   0   66
+];
+[diagg, env, env_col, env_lin] = col_envelope(A)
+[diagg, env, env_col, env_lin] = lin_envelope(A')
+
+
 A = [
     11  12  13  14  15  16;
     0   22  0   0   0   0;
