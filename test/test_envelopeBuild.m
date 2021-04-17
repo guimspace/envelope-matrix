31a3abee023b4b0dd303923e45d7ab1c70ade480
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-04-17 15:47:50 -0300

    Load sample matrices from script

--- test/test_envelopeBuild.m
+++ test/test_envelopeBuild.m
@@ -1,48 +1,12 @@
 addpath('../src');
+run('matrices.m');
 
-A = eye(6);
-[diagg, enve, enveCol, enveLin] = envelopeBuild(A)
+p = 1;
+n = size(matrices)(1);
 
+while (p < n)
+    A = matrices{p};
+    [diagg, enve, enveCol, enveLin] = envelopeBuild(A)
 
-A = [
-    11  12  0   14  0   0;
-    0   22  23  0   0   0;
-    0   0   33  0   0   0;
-    0   0   0   44  0   46;
-    0   0   0   0   55  0;
-    0   0   0   0   0   66
-];
-[diagg, enve, enveCol, enveLin] = envelopeBuild(A)
-
-
-A = [
-    11  12  0   0   0   0;
-    0   22  23  0   0   0;
-    0   0   33  0   0   0;
-    0   0   0   44  0   46;
-    0   0   0   0   55  0;
-    0   0   0   0   0   66
-];
-[diagg, enve, enveCol, enveLin] = envelopeBuild(A)
-
-
-A = [
-    11  12  0   0   0   0;
-    0   22  23  0   0   0;
-    0   0   33  34  0   0;
-    0   0   0   44  45  0;
-    0   0   0   0   55  56;
-    0   0   0   0   0   66
-];
-[diagg, enve, enveCol, enveLin] = envelopeBuild(A)
-
-
-A = [
-    11  12  13  14  15  16;
-    0   22  0   0   0   0;
-    0   0   33  0   0   0;
-    0   0   0   44  0   0;
-    0   0   0   0   55  0;
-    0   0   0   0   0   66
-];
-[diagg, enve, enveCol, enveLin] = envelopeBuild(A)
+    p = p + 1;
+end
