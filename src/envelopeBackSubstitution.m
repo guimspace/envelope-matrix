78f76835c4b5043a8027489c02c3f13415d5bb93
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-04-19 16:30:27 -0300

    Rename envelopeSubstitution() to envelopeBackSubstitution()

--- src/envelopeBackSubstitution.m
+++ src/envelopeBackSubstitution.m
@@ -18,7 +18,7 @@
 %	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 %	SOFTWARE.
 
-function x = envelopeSubstitution (envelope, b)
+function x = envelopeBackSubstitution (envelope, b)
     diagg = envelope{1};
     enve = envelope{2};
     enveCol = envelope{3};
