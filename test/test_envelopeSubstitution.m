6edc110ddeecfe0cac8f55ea4bd469c869f4e5ae
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-04-19 16:31:03 -0300

    Add test case for substitution

---
+++ test/test_envelopeSubstitution.m
@@ -0,0 +1,15 @@
+clear all
+addpath('../src');
+
+n = 6;
+
+U = triu(randi(9, n));
+L = eye(n) + tril(randi(9, n), -1);
+b = (L * U) * [1:n]';
+
+
+envelope = envelopeBuild(L');
+y = envelopeForwardSubstitution(envelope, b);
+
+envelope = envelopeBuild(U);
+x = envelopeBackSubstitution(envelope, y)
