61d5e23ef0f90781df7f92715a129152bd701c58
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-05-05 12:02:27 -0300

    Split test runtime in parts

--- c/test.c
+++ c/test.c
@@ -5,23 +5,59 @@
 #include "wrap.h"
 #include "solve.h"
 
+void test_tools();
+void test_substitution();
+
 int main(int argc, char **argv)
 {
-        int n, i, j;
+        test_tools();
+        test_substitution();
 
+	return 0;
+}
+
+void test_substitution()
+{
+        float *triU = (float[17]){4, 1, 1, 0, 3, 0, -1, -1, -5, 0, 0, 3, 13, 0, 0, 0, -13};
+        float vecB[4] = {8, -9, 26, -26};
+        float *solX;
+        int n, i;
+
+        n = triU[0];
+        env envelope = init_envelope(n);
+        triU += 1;
+
+        build_envelope(envelope, triU);
+
+        solX = solve_back_substitution(envelope, vecB);
+        for (i = 0; i < n; i++)
+                printf("%.2f\n", solX[i]);
+        free(solX);
+        printf("\n");
+
+        solX = solve_forward_substitution(envelope, vecB);
+        for (i = 0; i < 4; i++)
+                printf("%.2f\n", solX[i]);
+        free(solX);
+        printf("\n");
+
+        end_envelope(envelope);
+}
+
+void test_tools()
+{
         float *matrix = (float[37]){6, 11, 12, 0, 14, 0, 0, 0, 22, 23, 0, 0, 0, 0, 0, 33, 0, 0, 0, 0, 0, 0, 44, 0, 46, 0, 0, 0, 0, 55, 0, 0, 0, 0, 0, 0, 66};
+
+        env envelope = init_envelope(matrix[0]);
         matrix += 1;
 
-        env envelope = init_envelope((matrix - 1)[0]);
-
         build_envelope(envelope, matrix);
-
         print_envelope(envelope);
         printf("\n");
 
-        i = 0;
-        j = 0;
-        n = (matrix - 1)[0];
+        int i = 0;
+        int j = 0;
+        int n = (matrix - 1)[0];
         while (i < n) {
                 if (get_element(envelope, i, j) != matrix[i * n + j]) {
                         printf("%.2f != %.2f\tat ", get_element(envelope, i, j), matrix[i * n + j]);
@@ -37,43 +73,16 @@ int main(int argc, char **argv)
         }
         printf("\n");
 
-        float *matrix2 = unwrap_envelope(envelope);
+        float *regen = unwrap_envelope(envelope);
 
-        int l = (int)matrix2[0];
+        int l = (int)regen[0];
         n = l * l;
-        for (i = 1; i <= n; ) {
-                printf("%.2f\t", matrix2[i]);
+        for (i = 1; i <= n; i++) {
+                printf("%.2f\t", regen[i]);
                 if (i % l == 0)
                         printf("\n");
-                i++;
         }
 
         end_envelope(envelope);
         printf("\n");
-
-        float *triU = (float[17]){4, 1, 1, 0, 3, 0, -1, -1, -5, 0, 0, 3, 13, 0, 0, 0, -13};
-        triU += 1;
-
-        envelope = init_envelope((triU - 1)[0]);
-
-        build_envelope(envelope, triU);
-
-        float vecB[4] = {8, -9, 26, -26};
-        float *solX;
-
-        solX = solve_back_substitution(envelope, vecB);
-
-        for (i = 0; i < 4; i++)
-                printf("%.2f\n", solX[i]);
-        free(solX);
-        printf("\n");
-
-        solX = solve_forward_substitution(envelope, vecB);
-
-        for (i = 0; i < 4; i++)
-                printf("%.2f\n", solX[i]);
-        free(solX);
-        end_envelope(envelope);
-
-	return 0;
 }
