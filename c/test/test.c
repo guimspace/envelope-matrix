cc7cb5843aa5f26c03d36148211561908f8565c5
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-05-12 16:23:15 -0300

    Separate C files in src/ and test/

--- c/test/test.c
+++ c/test/test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "utils.h"
-#include "wrap.h"
-#include "solve.h"
+#include "../src/utils.h"
+#include "../src/wrap.h"
+#include "../src/solve.h"
 
 void test_utils();
 void test_substitution();
