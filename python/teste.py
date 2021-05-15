6284599f32b484a1ab3206cf2807181b114edbbe
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-05-15 00:24:16 -0300

    Update teste.py
    Signed-off-by: Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>

--- python/teste.py
+++ python/teste.py
@@ -19,7 +19,7 @@ i = 0
 j = 0
 while(i < n):
     if j < i:
-        print(f'{""}\t', end = "")
+        print(f'{""}\t', end = "")  #format matrix
         j = j + 1
     elif j < n:
         print(f'{matrix[i][j]}\t', end = "")
@@ -37,10 +37,31 @@ envelope = []
 i = 0
 j = 0
 while(j < n):
-    while(i < j and matrix[i][j] == 0):
+    while(i < j and matrix[i][j] == 0):  #skips 0 before an whole number
         i = i + 1
-    while(i != j):
+    while(i != j): #print the numbers before the diagonal
+        envelope.append(matrix[i][j])
         print(f'{matrix[i][j]}\t', end = "")
         i = i + 1
     j = j + 1
     i = 0
+
+envelope.append(0)
+print("0")
+
+
+
+print("")
+
+
+print("Matrix diagonal")
+i = 0
+
+while(i < n):
+    print(f'{matrix[i][i]}\t', end = "")
+    i = i + 1
+
+
+print("")
+
+print("Envcol")
