cf2cc6c0fb92037404e845e17fba2f8a73e21e14
Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>
2021-05-16 19:11:13 -0300

    Rename function envMatrix() to generateEnvelope()
    Signed-off-by: Igor Costa Carvalho Campos Silva <igorsilvabhz6@gmail.com>

--- python/wrap.py
+++ python/wrap.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-def envMatrix(matrix):
+def generateEnvelope(matrix):
     ENVcol = []
     envelope = []
     ENVlin = []
